86bfeb90a8e720576289af2b3d4edf7b1b7199ab
Aleksander Płomiński <plominski.aleksander@gmail.com>
2025-12-23 16:57:06 +0100

    v2 - Added more exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { NodePlatform } from "./components/NodePlatform/NodePlatform";
 export { NodeSystem, type NodeSystemProps, type NodeSystemHandle } from "./components/NodeSystem/NodeSystem";
 export { Node } from "./components/Node/Node.tsx";
+export type { Connection, NodeData, NodeTypeDef } from "./components/Node/node.ts";
 export { serializeGraph } from "./utils/serialize";
 export { deserializeGraph } from "./utils/deserialize";
